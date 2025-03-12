ff178ad9985cc3cadb123c631a5f1b6d4a9a1781
AaronPenner <rockcairncom@gmail.com>
2025-03-12 15:45:41 -0600

    use knex data migrations

---
+++ db/migrations/20250312213031_create_peaks_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('peaks', table => {
+    table.increments('id');
+    table.string('name');
+    table.integer('height');
+    table.string('mounain_range');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('peaks');
+};
---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+require('dotenv').config();
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+};
