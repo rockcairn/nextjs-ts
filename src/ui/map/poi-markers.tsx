5e5dd6ec8821684ea7b62dbccf9dcb74174eb8a6
AaronPenner <rockcairncom@gmail.com>
2025-04-08 21:36:19 -0600

    WIP: simplified the markers and references

--- src/ui/map/poi-markers.tsx
+++ src/ui/map/poi-markers.tsx
@@ -1,62 +1,49 @@
 'use client'
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 import type { Marker } from '@googlemaps/markerclusterer';
-import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import {
     AdvancedMarker,
     InfoWindow,
     Pin,
+    useAdvancedMarkerRef,
     useMap
 } from '@vis.gl/react-google-maps';
-import { boolean } from 'zod';
 
 export type Poi ={ key: string, domain: string, path: string, color: string, ele: number, location: google.maps.LatLngLiteral }
 
 export default function PoiMarkers(props: { pois: Poi[] }) {
     const map = useMap();
-    const [openInfo, setOpenInfo] = useState<string>();
-    const [markers, setMarkers] = useState<{[key: string]: Marker}>({});
-  
-    const setMarkerRef = (marker: Marker | null, key: string) => {
-      if (marker && markers[key]) return;
-      if (!marker && !markers[key]) return;
-  
-      setMarkers(prev => {
-        if (marker) {
-          return {...prev, [key]: marker};
-        } else {
-          const newMarkers = {...prev};
-          delete newMarkers[key];
-          return newMarkers;
-        }
-      });
-    };
+    const [openMarkerId, setOpenMarkerId] = useState<string | null>(null);
+    const markerRefs = useRef<Record<string, google.maps.marker.AdvancedMarkerElement | null>>({});
   
     return (
       <>
         {props.pois.map( (poi: Poi) => (
-        <div>
           <AdvancedMarker
             title={poi.key}
-            key={poi.key}
             position={poi.location}
-            ref={marker => setMarkerRef(marker, poi.key)}
+            key={poi.key}
+            ref={(ref) => { markerRefs.current[poi.key] = ref;}}
             clickable={true}
-            onClick={() => setOpenInfo(poi.key)}
+            // onClick={() => setOpenInfo(poi.key)}
+            onClick={() => setOpenMarkerId(poi.key)}
             >
               <Pin background={poi.color} glyphColor={'#000'} borderColor={'#000'} />
-          </AdvancedMarker>
-          {openInfo === poi.key && (<InfoWindow
-            anchor={markers[poi.key]}
+              {openMarkerId === poi.key && (<InfoWindow
+            // anchor={markers[poi.key]}
+            anchor={markerRefs.current[poi.key]!}
             maxWidth={200}
-            onCloseClick={() => setOpenInfo('')}
+            // onCloseClick={() => setOpenInfo('')}
+            onCloseClick={() => setOpenMarkerId(null)}
             headerContent={poi.key + " (" + poi.ele + " ft.)"} >
                 <div style={{textAlign: 'center', padding:'1px 2px 10px 2px'}}><a href={`${poi.domain}${poi.path}`} >Trip Report</a>
                 or (<a style={{fontSize: '12px'}} href="#map" onClick={() => {map!.panTo(poi.location); map!.setZoom(12);}} >focus map</a>) </div>
           </InfoWindow>
           )};
-        </div>
+          </AdvancedMarker>
+         
+        // </div>
         ))}
       </>
     );
